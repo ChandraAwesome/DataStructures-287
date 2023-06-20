d971aab3b744c3c3c273072514feac84755ecf26
ChandarAwesome <ajaygames00Gmail.com>
2023-06-20 16:03:01 +0530

    [Chandra]-LinkedList

---
+++ CustomLinkedList.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_287
+{ 
+    public class LinkedListNode<T>
+        {
+            public T Value { get; set; }
+            public LinkedListNode<T> Next { get; set; }
+
+            public LinkedListNode(T value)
+            {
+                Value = value;
+                Next = null;
+            }
+        }
+
+        public class CustomLinkedList<T>
+        {
+            private LinkedListNode<T> head;
+            private LinkedListNode<T> tail;
+
+            public int Count { get; private set; }
+
+            public void AddFirst(T value)
+            {
+                LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+
+                if (head == null)
+                {
+                    head = newNode;
+                    tail = newNode;
+                }
+                else
+                {
+                    newNode.Next = head;
+                    head = newNode;
+                }
+
+                Count++;
+            }
+
+            public void AddLast(T value)
+            {
+                LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+
+                if (tail == null)
+                {
+                    head = newNode;
+                    tail = newNode;
+                }
+                else
+                {
+                    tail.Next = newNode;
+                    tail = newNode;
+                }
+
+                Count++;
+            }
+
+            public void RemoveFirst()
+            {
+                if (head == null)
+                {
+                    throw new InvalidOperationException("The LinkedList is empty.");
+                }
+
+                if (head == tail)
+                {
+                    head = null;
+                    tail = null;
+                }
+                else
+                {
+                    head = head.Next;
+                }
+
+                Count--;
+            }
+
+            public void RemoveLast()
+            {
+                if (tail == null)
+                {
+                    throw new InvalidOperationException("The LinkedList is empty.");
+                }
+
+                if (head == tail)
+                {
+                    head = null;
+                    tail = null;
+                }
+                else
+                {
+                    LinkedListNode<T> currentNode = head;
+                    while (currentNode.Next != tail)
+                    {
+                        currentNode = currentNode.Next;
+                    }
+                    currentNode.Next = null;
+                    tail = currentNode;
+                }
+
+                Count--;
+            }
+
+            public void Print()
+            {
+                LinkedListNode<T> currentNode = head;
+                while (currentNode != null)
+                {
+                    Console.WriteLine(currentNode.Value);
+                    currentNode = currentNode.Next;
+                }
+            }
+        }
+
+    }
+
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿namespace DataStructures_287
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            CustomLinkedList<int> linkedList = new CustomLinkedList<int>();
+
+            linkedList.AddFirst(3);
+            linkedList.AddFirst(2);
+            linkedList.AddFirst(1);
+
+            linkedList.AddLast(4);
+            linkedList.AddLast(5);
+
+            linkedList.Print();
+
+            linkedList.RemoveFirst();
+            linkedList.RemoveLast();
+
+            linkedList.Print();
+
+            /***********************************************/
+
+            CustomStack<int> stack = new CustomStack<int>(5);
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            stack.Print();
+
+            int topItem = stack.Peek();
+            Console.WriteLine("Top item: " + topItem);
+
+            int poppedItem = stack.Pop();
+            Console.WriteLine("Popped item: " + poppedItem);
+
+            stack.Print();
+
+            stack.Clear();
+            Console.WriteLine("Stack cleared.");
+
+            stack.Print();
+
+            /*********************************************************/
+
+            CustomQueue<int> queue = new CustomQueue<int>(5);
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            queue.Print();
+
+            int frontItem = queue.Peek();
+            Console.WriteLine("Front item: " + frontItem);
+
+            int dequeuedItem = queue.Dequeue();
+            Console.WriteLine("Dequeued item: " + dequeuedItem);
+
+            queue.Print();
+
+            queue.Clear();
+            Console.WriteLine("Queue cleared.");
+
+            queue.Print();
+
+            /***********************************************************/
+
+            CRUDwithLinkedList.CRUD1();
+            CRUDwithStack.CRUD2();
+            CRUDwithQueue.CRUD3();
+
+
+        }
+    }
+}
